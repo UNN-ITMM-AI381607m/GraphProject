46712b274e2e658d5f0168d06a6d3a5778d5e568
Elizaveta Pronina <elizaveta.pronina@intel.com>
2017-05-18 06:59:37 +0300

    added files to resources  (to do: move to GUI)

--- GraphComponent/PopupWindow/TheoryWindow.xaml.cs
+++ GraphComponent/PopupWindow/TheoryWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.IO;
+
 
 namespace GraphComponent.PopupWindow
 {
@@ -28,19 +19,19 @@ namespace GraphComponent.PopupWindow
         private void PropertiesButton_Click(object sender, RoutedEventArgs e)
         {
             //TheoryRichTextBox.Document.Blocks.Clear();
-            TheoryRichTextBox.Text = (File.ReadAllText("C:\\Users\\Александра\\Documents\\git\\GraphProject\\Theory\\1.txt"));
+            TheoryRichTextBox.Text = Properties.Resources._1;
         }
 
         private void FlatButton_Click(object sender, RoutedEventArgs e)
         {
             //TheoryRichTextBox.Document.Blocks.Clear();
-            TheoryRichTextBox.Text = (File.ReadAllText("C:\\Users\\Александра\\Documents\\git\\GraphProject\\Theory\\2.txt"));
+            TheoryRichTextBox.Text = Properties.Resources._2;
         }
 
         private void EstimatesButton_Click(object sender, RoutedEventArgs e)
         {
             //TheoryRichTextBox.Document.Blocks.Clear();
-            TheoryRichTextBox.Text = (File.ReadAllText("C:\\Users\\Александра\\Documents\\git\\GraphProject\\Theory\\3.txt"));
+            TheoryRichTextBox.Text = Properties.Resources._3;
             //TheoryRichTextBox.AppendText("EstimatesButton_Click");
         }
 
