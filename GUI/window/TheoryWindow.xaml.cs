82d569a4fa7c3819349b3e3e908589d458c7b238
Elizaveta Pronina <elizaveta.pronina@intel.com>
2017-05-20 00:32:14 +0300

    Moved Theory Window to GUI

--- GUI/window/TheoryWindow.xaml.cs
+++ GUI/window/TheoryWindow.xaml.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
-using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
-
-namespace GraphComponent.PopupWindow
+namespace GUI
 {
     /// <summary>
     /// Interaction logic for TheoryWindow.xaml
